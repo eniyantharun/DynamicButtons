f9a211bf78c43ddfe9e27639d10f56c5123ab7af
tharuneniyan <tharuneniyan@gmail.com>
2019-02-14 00:24:46 +0530

    Initial commit

---
+++ app/src/main/java/com/tharuneniyan/dynamicbuttons/MainActivity.java
@@ -0,0 +1,77 @@
+package com.tharuneniyan.dynamicbuttons;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1, button2, button3;
+    int[] inputArray = new int[] {1,2,3,4,5};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+
+        button1.setText("1");
+        button2.setText("2");
+        button3.setText("3");
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rightRotate(inputArray, 1);
+                buttonNameSet(inputArray);
+
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rightRotate(inputArray, 1);
+                buttonNameSet(inputArray);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rightRotate(inputArray, 1);
+                buttonNameSet(inputArray);
+            }
+        });
+
+    }
+
+    private void rightRotate(int[] inputArray, int n) {
+
+        Log.d(MainActivity.class.getSimpleName(),"inputArrayf"+Arrays.toString(inputArray));
+
+        int temp;
+        for (int i = 1; i <= n; i++)
+        {
+            temp = inputArray[inputArray.length-1];
+            for (int j = inputArray.length-1; j > 0; j--)
+            {
+                inputArray[j] = inputArray[j-1];
+            }
+            inputArray[0] = temp;
+        }
+
+        Log.d(MainActivity.class.getSimpleName(),"inputArrayf"+Arrays.toString(inputArray));
+    }
+
+    private void buttonNameSet(int[] inputArray) {
+        button1.setText(String.valueOf(inputArray[0]));
+        button2.setText(String.valueOf(inputArray[1]));
+        button3.setText(String.valueOf(inputArray[2]));
+    }
+}
+
